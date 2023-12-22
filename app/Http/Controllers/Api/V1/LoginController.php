5c3cb115353f121bc24df5d312546562a488f27a
amirsahra <amirhosein.sahra@gmail.com>
2023-12-22 20:51:25 +0330

    fix(auth): login controller

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
     public function __invoke(LoginRequest $request): void
     {
         $user = User::where('email',$request->email)->first();
-        if (!$user || !Hash::check($request->password. $user->password)){
+        if (!$user || !Hash::check($request->password, $user->password)){
             throw ValidationException::withMessages([
                 'email' => ['The credentials you entered are incorrect.']
             ]);
