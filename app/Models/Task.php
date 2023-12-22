931d1322c60e870c5e868f060283a159f7afad64
amirsahra <amirhosein.sahra@gmail.com>
2023-12-22 17:16:17 +0330

    feat(task): completed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed|true $is_completed
+ * @property string $name
+ * @property integer $id
+ */
 class Task extends Model
 {
     use HasFactory;
