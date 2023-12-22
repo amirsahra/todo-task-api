6686ea7759c26bb4d88512a341b57d7f77c500ff
amirsahra <amirhosein.sahra@gmail.com>
2023-12-22 20:52:19 +0330

    fix(auth): user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,7 +12,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
@@ -28,7 +28,7 @@ class UserFactory extends Factory
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified()
+    public function unverified(): Factory
     {
         return $this->state(function (array $attributes) {
             return [
