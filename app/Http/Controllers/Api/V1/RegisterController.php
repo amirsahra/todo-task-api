b511c1676261b89ad2709eafbd0de8df2892906b
amirsahra <amirhosein.sahra@gmail.com>
2023-12-23 19:55:51 +0330

    feat(auth): register controller

---
+++ app/Http/Controllers/Api/V1/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
