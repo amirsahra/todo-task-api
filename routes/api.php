d57a69ecad4b5e23dc49ddb86992a690574f67c4
amirsahra <amirhosein.sahra@gmail.com>
2023-12-23 19:53:56 +0330

    feat(auth): logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\LoginController;
+use App\Http\Controllers\Api\V1\LogoutController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('V1')->group(function () {
     Route::post('login', LoginController::class);
+    Route::post('logout', LogoutController::class);
     Route::apiResource('tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
