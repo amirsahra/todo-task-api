e771bcd70f9f7a54a0ad1554493a9593ac0d5b46
amirsahra <amirhosein.sahra@gmail.com>
2023-12-26 18:21:54 +0330

    refactor(auth): add customJson response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Response::macro('customJson', function ($data = [], $message = 'Success', $status = 200) {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data,
+            ], $status);
+        });
     }
 }
